85b8b8878356eae5ada7a853ae12e9d970c9ff7f
kmei <kelly.mei@uftwf.org>
2023-01-31 16:52:43 -0500

    gtm environment setup

---
+++ src/main/webapp/WEB-INF/resources/scripts/uftwf-gtm-environment.js
@@ -0,0 +1,25 @@
+<!-- Google Tag Manager -->
+<!-- Check the hostname and branch the prod and dev environment tags -->
+
+if(window.location.hostname == "services.uft.org"){
+    <!-- Production -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl+ '&gtm_auth=i91GxXrplktTDko6B98qsQ&gtm_preview=env-1&gtm_cookies_win=x';f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-KFBLWL2');</script>
+}
+
+else
+
+{
+    <!-- Dev -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl+ '&gtm_auth=SBk6g4kpsJltp1SnmBQ_Fw&gtm_preview=env-3&gtm_cookies_win=x';f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-KFBLWL2');</script>
+}
+
+
+
